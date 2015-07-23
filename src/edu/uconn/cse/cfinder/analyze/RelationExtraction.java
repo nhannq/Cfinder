e378a4a12901a7b7e8de1c409e77d4baa94a7965
Nhan Nguyen <nhannq88@gmail.com>
2015-07-23 16:59:48 -0400

    Add RelationExtraction

---
+++ src/edu/uconn/cse/cfinder/analyze/RelationExtraction.java
@@ -0,0 +1,9 @@
+package edu.uconn.cse.cfinder.analyze;
+
+import edu.uconn.cse.ccg.model.CCGraph;
+
+public class RelationExtraction {
+  public static void extractRelation(CCGraph graph) {
+
+  }
+}
